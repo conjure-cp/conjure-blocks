e312c138b616130e1356a3d6cd07b320f0d5d286
Nadine Martin <nm275@st-andrews.ac.uk>
2025-11-06 19:07:15 +0000

    better messages, especially for mutator blocks

--- src/index.js
+++ src/index.js
@@ -16,6 +16,7 @@ import {jsonToolbox} from './jsonToolbox';
 import './index.css';
 import {essenceBlocks} from './blocks/automatedBlocks';
 import { autoToolbox } from './blocks/automatedBlocks';
+import { blocks } from 'blockly/blocks';
 
 /*console.log(essenceBlocks);
 for (let b of essenceBlocks){
@@ -228,20 +229,37 @@ ws.addChangeListener((e) => {
   runCode();
 });
 
+// change listener to add comment to describe the required input types on block creation.
 ws.addChangeListener((e) => {
+
   if (e.type == Blockly.Events.BLOCK_CREATE) {
     for (let b of e.ids){
       let block = ws.getBlockById(b);
-      console.log(block.inputList);
       let types = "";
-      for (let i in block.inputList) {
-        console.log(i);
-        if (block.inputList[i].connection){
-          types = types + i +":" + block.inputList[i].connection.getCheck() +",";
-        }
+      let slot = 1;
+      
+      // if has a mutator - i.e a list block, individual description for all inputs
+      if (block.mutator){
+        types = "Click cog to change number of inputs. Each input requires a '" + block.inputList[1].connection.getCheck() + "' block.";
+      } 
+      else {
+        // build description labelling input with types
+         for (let i in block.inputList) {
+            if (block.inputList[i].connection){
+              types = types + "input " + slot +": " + block.inputList[i].connection.getCheck() +"\n";
+              slot += 1;
+            }
         
+        }
       }
-      block.setCommentText(types);
+     
+      
+      // no comment if no inputs.
+      if (types.length > 0){
+        block.setCommentText(types);
+      }
+      
+
     }
   }
 })
