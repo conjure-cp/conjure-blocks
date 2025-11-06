a64a9e37202047f7f501ee7d1b49ad646536f524
Nadine Martin <nm275@st-andrews.ac.uk>
2025-11-06 19:24:50 +0000

    stopped comments from being editable - whole block not editable

--- src/index.js
+++ src/index.js
@@ -257,6 +257,7 @@ ws.addChangeListener((e) => {
       // no comment if no inputs.
       if (types.length > 0){
         block.setCommentText(types);
+        block.setEditable(false);
       }
       
 
